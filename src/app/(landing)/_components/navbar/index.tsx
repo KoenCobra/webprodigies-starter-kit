61dbb0256bb10b5629141d7460fe6a62838fad2d
Koen De Groot <koendegroot84@gmail.com>
2024-12-27 09:17:36 +0100

    added icons

---
+++ src/app/(landing)/_components/navbar/index.tsx
@@ -0,0 +1,9 @@
+const LandingPageNavbar = () => {
+    return (
+        <div className="flex w-full justify-between sticky top-0 items-center py-5 z-50">
+            <p className="font-bold text-2xl">Grouple.</p>
+        </div>
+    )
+}
+
+export default LandingPageNavbar
---
+++ src/constants/index.ts
@@ -0,0 +1,8 @@
+type GroupleConstantsProps = {
+    landingPageMenu: MenuProps[]
+    signUpForm: AuthFormProps[]
+    signInForm: AuthFormProps[]
+    groupList: GroupListProps[]
+    createGroupPlaceholder: CreateGroupPlaceholderProps[]
+    groupPageMenu: GroupMenuProps[]
+}
